c61cad32e352c68de52ba7363c653d946ae49533
Yue Li <yue.li@salesforce.com>
2021-03-04 00:36:22 -0800

    v1 of metric learner and tests

--- src/fillmore/metric_learners.py
+++ src/fillmore/metric_learners.py
@@ -2,17 +2,94 @@ import tensorflow as tf
 
 class MetricLearner(tf.keras.Model):
     """A learner that uses a learned distance metric to make predictions."""
-    def call(self):
-        pass
 
-    def compute_loss(self):
-        pass
+    def __init__(self, embedding_func, **kwargs):
+        # overwrite base class attributes
+        super(MetricLearner, self).__init__(**kwargs)
+        self.embedding_func = embedding_func
+    
+    def call(self, episode):
+        support_examples = episode["support_examples"]
+        query_examples = episode["query_examples"]
+        support_labels_onehot = episode["support_labels_onehot"]
+        support_embeddings = self.embedding_func(support_examples)
+        query_embeddings = self.embedding_func(query_examples)
+        query_logits = self.compute_logits_for_episode(support_embeddings, query_embeddings, support_labels_onehot)
+        return query_logits
 
-    def compute_logits_for_episode(self):
-        pass
+    def compute_loss(self, query_logits, query_labels_onehot):
+        """generate loss for an episode
 
-    def compute_accuracy(self):
-        pass
+        Args:
+            query_logits ([N*Q, N]): query logits from `compute_logits_for_episode`
+            query_labels_onehot ([N*Q, N]): onehot label for query
 
-    def embedding_func(self):
-        pass
+        Returns:
+            cost [float]: cross entropy loss averaged over all query examples
+        """
+        cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(
+        query_labels_onehot, query_logits)
+
+        cost = tf.reduce_mean(cross_entropy_loss)
+        return cost
+
+    def compute_accuracy(self, query_labels_onehot, query_logits):
+        """[summary]
+
+        Args:
+            query_labels_onehot ([N*Q, N]): onehot label for query
+            query_logits ([N*Q, N]): query logits from `compute_logits_for_episode`
+
+        Returns:
+            accuracy [float]: accuracy averaged over all query examples
+        """
+        correct = tf.equal(tf.argmax(query_labels_onehot, -1), tf.argmax(query_logits, -1))
+        correct = tf.cast(correct, tf.float32)
+        accuracy = tf.reduce_mean(correct)
+        return accuracy
+
+    def _compute_prototypes(self, embeddings, labels):
+        """Computes class prototypes over the last dimension of embeddings.
+        Args:
+            embeddings: Tensor of examples of shape [num_examples, embedding_size].
+            labels: Tensor of one-hot encoded labels of shape [num_examples,
+            num_classes].
+        Returns:
+            prototypes: Tensor of class prototypes of shape [num_classes,
+            embedding_size].
+        """
+        labels = tf.cast(labels, tf.float32)
+
+        # [num examples, 1, embedding size].
+        embeddings = tf.expand_dims(embeddings, 1)
+
+        # [num examples, num classes, 1].
+        labels = tf.expand_dims(labels, 2)
+
+        # Sums each class' embeddings. [num classes, embedding size].
+        class_sums = tf.reduce_sum(labels * embeddings, 0)
+
+        # The prototype of each class is the averaged embedding of its examples.
+        class_num_images = tf.reduce_sum(labels, 0)  # [way].
+        prototypes = class_sums / class_num_images
+
+        return prototypes
+    
+    def compute_logits_for_episode(self, support_embeddings, query_embeddings, support_labels_onehot):
+        """calculates the prototype network logits (before softmax layer) using the latent representation of
+           support set and query set
+
+        Args:
+            support_embeddings ([N*S, D]): the latent representation of support set
+            query_embeddings ([N*Q, D]): the latent representation of query set
+            support_labels_onehot ([N*S, N]): the one-hot encodings of the query labels
+        Returns:
+            query_logits ([N*Q, N]): the query logits
+        """
+        prototypes = self._compute_prototypes(support_embeddings, support_labels_onehot)
+        
+        # compute the distance from the prototypes
+        prototypes = tf.expand_dims(prototypes, axis=0)  # [1, N, D]
+        query_embeddings = tf.expand_dims(query_embeddings, 1)  # [N*Q, 1, D]
+        distances = tf.reduce_sum(tf.square(query_embeddings - prototypes), 2)
+        return -distances
---
+++ tests/test_metric_learners.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from fillmore.metric_learners import MetricLearner
+from transformers import AutoConfig
+from fillmore.protonet import ProtoLoss
+from fillmore.bert_model import BertTextEncoderWrapper
+
+class MetricLearnerTest(tf.test.TestCase):
+    def setUp(self):
+        config = AutoConfig.from_pretrained("bert-base-uncased")
+        config.label_smoothing = 0.0
+        config.max_seq_len = 32
+        self.config = config
+        embedding_func = BertTextEncoderWrapper(self.config)
+        self.learner = MetricLearner(embedding_func)
+
+    def test_forward_pass(self):
+        episode = {
+            "support_examples": [
+                'am i allowed to put in a pto request for now to april',
+                'can you set up a pto request for me for march 20th to april 12th',
+                'can you put in a pto request for the 5th of june to june 8th',
+                'i need a pto request for the dates jan 15th to jan 20th',
+                'i need to put in a pto request from the 8th to the 17th of january'
+            ],
+            "query_examples": [
+                'i need to put in a pto request from the 8th to the 17th of january',
+                'can you put in a pto request for the 5th of june to june 8th'
+            ],
+            "support_labels_onehot": tf.one_hot([0, 0, 1, 2, 1], 3)
+        }
+        query_logits = self.learner(episode)
+        self.assertAllEqual(query_logits.shape, [2, 3])
+    
+    def test_compute_logits(self):
+        support_embeddings = tf.constant([[[1.0, 1.0], [0.0, 1.0], [0.5, 0.5]], [[1.0, -1.0], [0.0, -1.0], [0.5, -0.5]]]) # [N*S, D]
+        support_embeddings = tf.reshape(support_embeddings, [2*3, 2])
+        query_embeddings = tf.constant([[[0.0, 0.5]], [[0.0, -0.5]]])
+        query_embeddings = tf.reshape(query_embeddings, [2*1, 2])
+        support_labels_onehot = tf.constant([[[1., 0.], [1., 0.], [1., 0.]], [[0., 1.], [0., 1.], [0., 1.]]])
+        support_labels_onehot = tf.reshape(support_labels_onehot, [2*3, 2])
+        query_labels_onehot = tf.constant([[[1., 0.]], [[1., 0.]]]) # [N, Q, N]
+        query_labels_onehot = tf.reshape(query_labels_onehot, [2*1, 2])
+        
+        query_logits = self.learner.compute_logits_for_episode(support_embeddings, query_embeddings, support_labels_onehot)
+        self.assertAllEqual(query_logits.shape, [2, 2])
+
+        loss = self.learner.compute_loss(query_logits, query_labels_onehot)
+        self.assertAllClose(loss, 1.006340)
+
+        acc = self.learner.compute_accuracy(query_labels_onehot, query_logits)
+        self.assertAllClose(acc, 0.5)
+    
+    def test_proto_loss(self):
+        x_latent = tf.constant([[[1.0, 1.0], [0.0, 1.0], [0.5, 0.5]], [[1.0, -1.0], [0.0, -1.0], [0.5, -0.5]]]) # [N*S, D]
+        x_latent = tf.reshape(x_latent, [2*3, 2])
+        q_latent = tf.constant([[[0.0, 0.5]], [[0.0, -0.5]]])
+        q_latent = tf.reshape(q_latent, [2*1, 2])
+        num_classes = 2
+        num_support = 3
+        num_queries = 1
+        query_labels_onehot = tf.constant([[[1., 0.]], [[1., 0.]]]) # [N, Q, N]
+        support_labels_onehot = tf.constant([[[1., 0.], [1., 0.], [1., 0.]], [[0., 1.], [0., 1.], [0., 1.]]])
+        support_labels_onehot = tf.reshape(support_labels_onehot, [2*3, 2])
+        loss, acc = ProtoLoss(x_latent, q_latent, query_labels_onehot, num_classes, num_support, num_queries, self.config)
+        print("loss : {}".format(loss)) # 1.0063408613204956
+        print("acc: {}".format(acc)) # 0.5
