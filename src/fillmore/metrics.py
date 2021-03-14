2150047863b87dc513b5573b69ca2c715f315e55
Yue Li <yue.li@salesforce.com>
2021-03-13 23:41:00 -0800

    add select by thresholds

--- src/fillmore/metrics.py
+++ src/fillmore/metrics.py
@@ -41,6 +41,7 @@ def in_domain_stats_episode(query_logits, query_labels_onehot, thresholds):
     query_conf_mask = tf.cast(tf.greater_equal(query_conf, thresholds), tf.float32) # [Q, T]
     query_pred = tf.multiply(query_pred, query_conf_mask) #[Q, T]
     query_label = tf.cast(tf.argmax(query_labels_onehot, -1), tf.float32) # [Q,]
+    query_label = tf.expand_dims(query_label, axis=-1) # [Q, 1]
     in_domain_correct = tf.equal(query_label, query_pred)
     in_domain_correct = tf.cast(in_domain_correct, tf.float32)
     oos_output = tf.cast(tf.less(query_conf, thresholds), tf.float32)
@@ -56,7 +57,10 @@ def oos_f1_batch(in_domain_correct_all, oos_correct_all, oos_output_all):
         oos_output_all (List[oos_output]): [B, Qin, T]
 
     Returns:
-        in_domain_acc [type]: [description]
+        in_domain_acc [np.array of (T, )]: Cin/Nin
+        oos_recall [np.array of (T, )]: Coos/Noos
+        oos_precision [np.array of (T, )]: Coos/Noos_output
+        oos_f1 [np.array of (T, )]: 2*Roos*Poos/(Roos+Poos)
     """
     in_domain_correct_all = np.vstack(in_domain_correct_all)
     oos_correct_all = np.vstack(oos_correct_all)
@@ -69,22 +73,41 @@ def oos_f1_batch(in_domain_correct_all, oos_correct_all, oos_output_all):
     oos_correct = np.sum(oos_correct_all, axis=0, keepdims=True) # [1, T]
     oos_total = np.sum(oos_total_all, axis=0, keepdims=True) + np.finfo(np.float32).eps # [1,T]
     oos_precision = np.mean(np.multiply(oos_correct, 1.0/oos_total), axis=0)
-    return in_domain_acc, oos_recall, oos_precision
+    oos_f1 = np.multiply(2 * oos_recall, oos_precision)/(oos_recall + oos_precision + np.finfo(np.float32).eps)
+    return {
+        "in_domain_accuracies": in_domain_acc, 
+        "oos_recalls": oos_recall, 
+        "oos_precisions": oos_precision, 
+        "oos_f1s": oos_f1
+    }
+
+def select_oos_threshold(oos_stats, thresholds):
+    j_in_oos = oos_stats["in_domain_accuracies"] + oos_stats["oos_recalls"]
+    i = np.argmax(j_in_oos, axis=0)
+    best_threshold = thresholds[i]
+    return {
+        "threshold": best_threshold, 
+        "oos_recall": oos_stats["oos_recalls"][i], 
+        "in_domain_accuracy": oos_stats["in_domain_accuracies"][i],
+        "oos_precision": oos_stats["oos_precisions"][i],
+        "oos_f1": oos_stats["oos_f1s"][i]
+    }
 
 if __name__ == "__main__":
     query_logits = tf.constant([[5.0, 2.0, 1.0], [2.0, 3.0, 1.0]])
     query_labels_onehot = tf.constant([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
     thresholds = [0.5, 0.7]
     in_domain_correct, oos_output = in_domain_stats_episode(query_logits, query_labels_onehot, thresholds)
-    print(in_domain_correct) # [[1., 0.],[0., 0.]]
+    print(in_domain_correct) # [[1., 1.],[1., 0.]]
     print(oos_output) # [[0., 0.],[0., 1.]]
     oos_logits = tf.constant([[2.0, 3.0, 1.0]])
     oos_correct = oos_stats_episode(oos_logits, thresholds)
-    print(oos_correct) # [[0., 0.],[0., 1.]]
-    in_domain_acc, oos_recall, oos_precision = oos_f1_batch(
+    print(oos_correct) # [[0., 1.]]
+    oos_stats = oos_f1_batch(
         [in_domain_correct],
         [oos_correct],
         [oos_output])
-    ## when threshold = 0.5, Cin=1, Nin=2, Coos=0, Noos=1, acc_in=1/2, oos_precision=0/1, oos_recall=0/1
+    ## when threshold = 0.5, Cin=2, Nin=2, Coos=0, Noos=1, acc_in=1, oos_precision=0/1, oos_recall=0/1
     ## when threshold = 0.7, Cin=1, Nin=2, Coos=1, Noos=2, acc_in=1/2, oos_precision=1/2, oos_recall=1/1
+    stats = select_oos_threshold(oos_stats, thresholds)
     import pdb; pdb.set_trace()
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class MetricsTest(tf.test.TestCase):
     def setUp(self):
-        ## when threshold = 0.5, Cin=1, Nin=2, Coos=0, Noos=1, acc_in=1/2, oos_precision=0/1, oos_recall=0/1
+        ## when threshold = 0.5, Cin=2, Nin=2, Coos=0, Noos=1, acc_in=1, oos_precision=0/1, oos_recall=0/1
         ## when threshold = 0.7, Cin=1, Nin=2, Coos=1, Noos=2, acc_in=1/2, oos_precision=1/2, oos_recall=1/1
         self.query_logits = tf.constant([[5.0, 2.0, 1.0], [2.0, 3.0, 1.0]])
         self.query_labels_onehot = tf.constant([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
@@ -13,7 +13,7 @@ class MetricsTest(tf.test.TestCase):
     
     def test_in_domain_stats_episode(self):
         in_domain_correct, oos_output = in_domain_stats_episode(self.query_logits, self.query_labels_onehot, self.thresholds)
-        self.assertAllClose(in_domain_correct, np.array([[1., 0.],[0., 0.]]))
+        self.assertAllClose(in_domain_correct, np.array([[1., 1.],[1., 0.]]))
         self.assertAllClose(oos_output, np.array([[0., 0.],[0., 1.]]))
         self.assertEqual(in_domain_correct.shape, [2,2])
         self.assertEqual(oos_output.shape, [2,2])
@@ -26,14 +26,14 @@ class MetricsTest(tf.test.TestCase):
     def test_oos_f1_batch(self):
         in_domain_correct, oos_output = in_domain_stats_episode(self.query_logits, self.query_labels_onehot, self.thresholds)
         oos_correct = oos_stats_episode(self.oos_logits, self.thresholds)
-        in_domain_acc, oos_recall, oos_precision = oos_f1_batch(
+        oos_stats = oos_f1_batch(
         [in_domain_correct],
         [oos_correct],
         [oos_output])
-        self.assertAllClose(in_domain_acc, np.array([0.5, 0.]))
-        self.assertAllClose(oos_recall, np.array([0., 1.]))
-        self.assertAllClose(oos_precision, np.array([0. , 0.5]))
-        self.assertEqual(in_domain_acc.shape, (2,))
-        self.assertEqual(oos_recall.shape, (2,))
-        self.assertEqual(oos_precision.shape, (2,))
+        self.assertAllClose(oos_stats["in_domain_accuracies"], np.array([1.0, 0.5]))
+        self.assertAllClose(oos_stats["oos_recalls"], np.array([0., 1.]))
+        self.assertAllClose(oos_stats["oos_precisions"], np.array([0. , 0.5]))
+        self.assertEqual(oos_stats["in_domain_accuracies"].shape, (2,))
+        self.assertEqual(oos_stats["oos_recalls"].shape, (2,))
+        self.assertEqual(oos_stats["oos_precisions"].shape, (2,))
         
