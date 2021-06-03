71ed5b04fb6280a39b238d24a5cf10dabcaa9f67
Yue Li <whereisdodo@gmail.com>
2021-06-03 02:28:52 -0700

    add knn with relevance

--- src/fillmore/metric_learners.py
+++ src/fillmore/metric_learners.py
@@ -186,3 +186,116 @@ class KNNLearner(tf.keras.Model):
         labels_pred = tf.gather(tf.constant(labels_x), indices)
         labels_prob = tf.one_hot(labels_pred, depth=num_classes)
         return labels_prob
+
+
+import tensorflow as tf
+
+
+class KNNLearner(tf.keras.Model):
+    """A learner that uses a learned distance metric to make predictions."""
+
+    def __init__(self, embedding_func, distance_type='l2', **kwargs):
+        # overwrite base class attributes
+        super(KNNLearner, self).__init__(**kwargs)
+        self.embedding_func = embedding_func
+        self.distance_type = distance_type
+    
+    def call(self, episode, query_examples=[]):
+        if query_examples:
+            episode["query_examples"] = query_examples
+        query_prob = self.compute_logits_for_episode(episode)
+        return query_prob
+  
+    def _compute_relevance(self, episode):
+        """compute relevance/similarity between each query and support example
+        """
+        support_examples = episode['support_examples']
+        query_examples = episode['query_examples']
+
+        text_pairs = []
+        for q_id in range(len(query_examples)):
+            for s_id in range(len(support_examples)):
+                text_pairs.append((query_examples[q_id], support_examples[s_id]))
+        
+        features = self.embedding_func.tokenizer.batch_encode_plus(
+            batch_text_or_text_pairs= text_pairs,
+            padding = 'max_length',
+            truncation= True,
+            max_length= self.embedding_func.max_seq_len,
+            return_token_type_ids=True,
+            return_tensors= 'tf'
+        )
+        logits = self.embedding_func.compute_logits(features)
+        prob = tf.nn.softmax(logits)
+        relevance = prob[:, 0]
+        relevance = tf.reshape(relevance, (len(query_examples), len(support_examples)))
+        return relevance
+
+    def compute_loss(self, query_prob, query_labels_onehot):
+        """generate loss for an episode
+
+        Args:
+            query_prob ([N*Q, N]): query probability from `compute_logits_for_episode`
+            query_labels_onehot ([N*Q, N]): onehot label for query
+            support_labels_onehot ([N*S, N]): onehot label for support
+
+        Returns:
+            cost [float]: cross entropy loss averaged over all query examples
+        """
+
+#         bce = tf.keras.losses.BinaryCrossentropy()
+        bce = tf.keras.losses.BinaryCrossentropy(from_logits=False, reduction=tf.keras.losses.Reduction.NONE)
+        cross_entropy_loss = bce(query_labels_onehot, query_prob)
+
+        cost = tf.reduce_mean(cross_entropy_loss)
+        return cost
+
+    def compute_accuracy(self, query_labels_onehot, query_prob):
+        """[summary]
+
+        Args:
+            query_labels_onehot ([N*Q, N]): onehot label for query
+            query_prob ([N*Q, N]): query probability from `compute_logits_for_episode`
+
+        Returns:
+            accuracy [float]: accuracy averaged over all query examples
+        """
+        correct = tf.equal(tf.argmax(query_labels_onehot, -1), tf.argmax(query_prob, -1), -1)
+        correct = tf.cast(correct, tf.float32)
+        accuracy = tf.reduce_mean(correct)
+        return accuracy
+      
+    def compute_logits_for_episode(self, episode):
+        """calculates the nearest neighbors prediction using the latent representation of
+           support set and query set
+
+        Args:
+            support_embeddings ([N*S, D]): the latent representation of support set
+            query_embeddings ([N*Q, D]): the latent representation of query set
+            support_labels_onehot ([N*S, N]): the one-hot encodings of the query labels
+        Returns:
+            query_logits ([N*Q, N]): the query logits
+        """
+        if self.distance_type == 'l2':
+            support_embeddings = self.embedding_func(episode["support_examples"])
+            query_embeddings = self.embedding_func(episode["query_examples"])
+            # [1, num_support, embed_dims]
+            emb_support = tf.expand_dims(support_embeddings, axis=0)
+            # [num_query, 1, embed_dims]
+            emb_query = tf.expand_dims(query_embeddings, axis=1)
+            # [num_query, num_support]
+            distance = tf.norm(emb_support - emb_query, axis=2)
+        elif self.distance_type == 'relevance':
+            distance = -self._compute_relevance(episode)
+        else:
+            raise ValueError('Distance must be l2 or cosine')
+
+        support_labels_onehot = episode["support_labels_onehot"]
+        _, indices = tf.nn.top_k(-distance, k=1)
+        indices = tf.squeeze(indices, axis=1)
+        labels_x = tf.argmax(support_labels_onehot, axis=1)
+
+        num_classes = support_labels_onehot.shape[1]
+        labels_pred = tf.gather(tf.constant(labels_x), indices)
+        labels_prob = tf.one_hot(labels_pred, depth=num_classes)
+        return labels_prob
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 # Build
 python setup.py bdist_wheel
-pip install --upgrade --force-reinstall dist/fillmore-0.2.5-py3-none-any.whl
+pip install --upgrade --force-reinstall dist/fillmore-0.2.6-py3-none-any.whl
 
 # Run tests with xml reports and code coverage
 # python -m unittest discover -s ./tests/
